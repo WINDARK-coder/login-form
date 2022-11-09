7a063bca5df5e0cfcb8a7101f493530832ad6ebd
Telman <113706135+WINDARK-coder@users.noreply.github.com>
2022-11-09 17:09:20 +0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-
+// it checks whether you logged in or not.
 function isLogged(){
     if(!isset($_SESSION['logged_in'])){
         $_SESSION['error_message'] = "Please login for enter admin";
