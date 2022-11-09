7218a97c438b85f99405ba4936cae24d15798ef3
Telman <113706135+WINDARK-coder@users.noreply.github.com>
2022-11-09 17:12:32 +0400

    Update login.php

--- login.php
+++ login.php
@@ -12,21 +12,16 @@ if(isset($_SESSION['logged_in'])){
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Log in</title>
-
-    <!-- Google Font: Source Sans Pro -->
+    <title>Log in</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
     <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
-    <!-- icheck bootstrap -->
     <link rel="stylesheet" href="assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- Theme style -->
     <link rel="stylesheet" href="assets/css/adminlte.min.css">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="../../index2.html"><b>DIV</b>BLOG</a>
+        <a><b>DIV</b>BLOG</a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
@@ -63,32 +58,16 @@ if(isset($_SESSION['logged_in'])){
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-8">
-                        <div class="icheck-primary">
-                            <input type="checkbox" id="remember" name="remember">
-                            <label for="remember">
-                                Remember Me
-                            </label>
-                        </div>
-                    </div>
-                    <!-- /.col -->
                     <div class="col-4">
                         <button type="submit" class="btn btn-primary btn-block">Sign In</button>
                     </div>
-                    <!-- /.col -->
                 </div>
             </form>
         </div>
-        <!-- /.login-card-body -->
     </div>
 </div>
-<!-- /.login-box -->
-
-<!-- jQuery -->
 <script src="assets/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
 <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
 <script src="assets/js/adminlte.min.js"></script>
 </body>
 </html>
