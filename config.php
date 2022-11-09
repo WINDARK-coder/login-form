d6215bfaafef868a6a601fe061bc4ee0b8d18439
Telman <113706135+WINDARK-coder@users.noreply.github.com>
2022-11-09 17:07:27 +0400

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 
-$db = mysqli_connect("localhost", "telman", "563289jmdatw", "div_blog");
+$db = mysqli_connect("localhost", "root", "", "database");
+// here you create your own database and add there admin name and password(with md5 function).
 
 // Check connection
 if (mysqli_connect_errno()) {
